3c751d3306233fe06ec7d1014b3b8172d9b1d537
qku <kilian.unterguggenberger@hu-berlin.de>
2023-08-31 09:27:05 +0200

    make inv var test understandable

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,10 +5,13 @@ from pleasant.util import inv_variance_weighting, get_spectral_diffusion_rates
 # TODO: try out a test fixture - maybe a measurement object with the example data?
 
 def test_inv_variance_weighting():
-    data = np.array([
-        [3.1, np.nan, 1.0, 2.2, 65.1, 7.4],
-        [0.1, 1, 0.2, 0.5, np.nan, 1.1]
-    ]).T
-    assert inv_variance_weighting(data) == (2.6952574957030997, 0.08776440549263095)
-
-
+    """ Compute the inverse variance weighted average in a simple case. """
+    n = 10
+    y = 1.0
+    y_std = 0.5
+    data = np.zeros((n, 2))
+    data[:, 0] = y
+    data[:, 1] = y_std
+    weighted_avg = y
+    weighted_std = y_std * np.sqrt(1 / n)
+    assert inv_variance_weighting(data) == (weighted_avg, weighted_std)
