7bc5b04e862c7562f34126a7d91216b3e4096f4b
qku <kilian.unterguggenberger@hu-berlin.de>
2023-09-03 23:05:59 +0200

    add test_loader_qudi

---
+++ tests/test_loader_qudi.py
@@ -0,0 +1,13 @@
+from pleasant.loaders.qudi import get_descriptions_in_folder, load_qudi_folder
+
+def test_get_descriptions_in_folder(datadir):
+    descriptions = get_descriptions_in_folder(datadir)
+    assert len(descriptions) == 1
+
+def test_load_qudi_folder(datadir):
+    measurements = load_qudi_folder(str(datadir), break_duration=0.15)
+    assert len(measurements) == 6
+
+def test_load_qudi_folder_description_contains(datadir):
+    measurements = load_qudi_folder(str(datadir), description_contains='foo')
+    assert len(measurements) == 0
