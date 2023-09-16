84e6102f0e80eb95f9f7642cd1bb19831d72a9e1
qku <kilian.unterguggenberger@hu-berlin.de>
2023-09-16 23:25:12 +0200

    setting up linting

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,28 @@
 import nox
+from nox_poetry import session
 
 
-@nox.session(python=["3.11", "3.10"])
+nox.options.sessions = "lint", "tests"
+
+
+@session(python=["3.11", "3.10", "3.9"])
 def tests(session):
-    session.run("poetry", "install", external=True)
+    session.install("pytest", "pytest-cov", "pytest-datadir", ".")
     session.run("pytest", "--cov")
+
+
+locations = "src", "tests", "noxfile.py"
+
+
+@session(python="3.11")
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
+    session.run("flake8", *args)
+
+
+@session(python="3.11")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
