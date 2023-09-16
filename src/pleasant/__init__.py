b5fea2f90abd30ab89a483867207457f0d658c24
qku <kilian.unterguggenberger@hu-berlin.de>
2023-09-16 23:13:58 +0200

    introduce __all__

--- src/pleasant/__init__.py
+++ src/pleasant/__init__.py
@@ -1,3 +1,5 @@
-from .util import *
+from .util import get_spectral_diffusion_rates, inv_variance_weighting
+
+__all__ = ["inv_variance_weighting", "get_spectral_diffusion_rates"]
 
 __version__ = "0.1.0"
