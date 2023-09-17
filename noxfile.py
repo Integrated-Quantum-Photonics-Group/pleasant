6498c0024aa1664a9be28c2cdfc7d0aa8324823b
qku <kilian.unterguggenberger@hu-berlin.de>
2023-09-17 11:22:06 +0200

    automatic typechecking setup

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 from nox_poetry import session
 
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "mypy", "tests"
 
 
 @session(python=["3.11", "3.10"])
@@ -17,7 +17,13 @@ locations = "src", "tests", "noxfile.py"
 @session(python="3.11")
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
+    session.install(
+        "flake8",
+        "flake8-annotations",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
     session.run("flake8", *args)
 
 
@@ -26,3 +32,10 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@session(python=["3.11", "3.10"])
+def mypy(session):
+    args = session.posargs or locations
+    session.install("mypy", "pandas-stubs")
+    session.run("mypy", *args)
