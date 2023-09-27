fcfcaf1c3016365a39df2f794dc459909bf5a280
qku <kilian.unterguggenberger@hu-berlin.de>
2023-09-27 20:42:57 +0200

    set up basic CI and docs CD (#4)
    * add tests workflow
    
    * add nox_poetry dependency
    
    * reference version from pyproject.toml
    
    * run docs per default
    
    * use githubpages ext
    
    * add docs workflow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- CD: Docs deployment to GitHub Pages.
+- CI: Testing workflow.
 - Module-level docstrings.
 - Docstrings to test suite and nox file.
 - Docstring linting.
--- src/pleasant/__init__.py
+++ src/pleasant/__init__.py
@@ -1,7 +1,9 @@
 """Routines for post-processing and analyzing PLE experiment data."""
 
+from importlib.metadata import version
+
 from .util import get_spectral_diffusion_rates, inv_variance_weighting
 
 __all__ = ["inv_variance_weighting", "get_spectral_diffusion_rates"]
 
-__version__ = "1.1.0"
+__version__ = version(__name__)
